1c5681237fb6e71837962255406bbee948e7841e
Sergio Milardovich <sergio.milardovich@gmail.com>
2015-03-30 09:03:46 -0300

    Gulpfile added

---
+++ Gulpfile.js
@@ -0,0 +1,31 @@
+var gulp = require('gulp');
+
+gulp.task('default', function () {
+    gulp.start('sass','js-copy','css-copy');
+});
+
+var sass = sass = require('gulp-sass');
+
+gulp.task('sass', function () {
+    gulp.src('./web/vendor/foundation/scss/foundation.scss')
+        .pipe(sass({sourceComments: 'map'}))
+        .pipe(gulp.dest('./web/css'));
+});
+
+
+gulp.task('js-copy', function() {
+    gulp.src([
+        './web/bundles/*/js/**/*.js',
+        './web/vendor/foundation/js/foundation.min.js',
+        './web/vendor/jquery/dist/jquery.min.js',
+        './web/vendor/requirejs/require.js'
+    ])
+        .pipe(gulp.dest('./web/js'));
+});
+
+gulp.task('css-copy', function() {
+    gulp.src([
+        './web/bundles/*/css/**/*.css'
+    ])
+        .pipe(gulp.dest('./web/css'));
+});
