1ed143d4d446765b1b135e550e47e610547b6112
Sergio Milardovich <sergio.milardovich@gmail.com>
2015-03-30 08:56:10 -0300

    Login system up and running

--- src/Galt/Bundle/MainBundle/Entity/Person.php
+++ src/Galt/Bundle/MainBundle/Entity/Person.php
@@ -33,17 +33,18 @@ class Person extends BaseUser
      */
     public function __construct()
     {
+        parent::__construct();
         $this->tickets = new \Doctrine\Common\Collections\ArrayCollection();
     }
 
     /**
-     * @ORM\OneToOne(targetEntity="Galt\Bundle\TicketBundle\Entity\Ticket", inversedBy="assigned")
+     * @ORM\OneToMany(targetEntity="Galt\Bundle\TicketBundle\Entity\Ticket", mappedBy="assigned")
      */
     protected $tickets;
 
 
     /**
-     * @ORM\OneToOne(targetEntity="Galt\Bundle\TicketBundle\Entity\Ticket", inversedBy="persons")
+     * @ORM\OneToMany(targetEntity="Galt\Bundle\TicketBundle\Entity\Ticket", mappedBy="persons")
      */
     protected $authored;
 
@@ -92,5 +93,51 @@ class Person extends BaseUser
     public function getAuthored()
     {
         return $this->authored;
+    }
+
+    /**
+     * Add tickets
+     *
+     * @param \Galt\Bundle\TicketBundle\Entity\Ticket $tickets
+     * @return Person
+     */
+    public function addTicket(\Galt\Bundle\TicketBundle\Entity\Ticket $tickets)
+    {
+        $this->tickets[] = $tickets;
+
+        return $this;
+    }
+
+    /**
+     * Remove tickets
+     *
+     * @param \Galt\Bundle\TicketBundle\Entity\Ticket $tickets
+     */
+    public function removeTicket(\Galt\Bundle\TicketBundle\Entity\Ticket $tickets)
+    {
+        $this->tickets->removeElement($tickets);
+    }
+
+    /**
+     * Add authored
+     *
+     * @param \Galt\Bundle\TicketBundle\Entity\Ticket $authored
+     * @return Person
+     */
+    public function addAuthored(\Galt\Bundle\TicketBundle\Entity\Ticket $authored)
+    {
+        $this->authored[] = $authored;
+
+        return $this;
+    }
+
+    /**
+     * Remove authored
+     *
+     * @param \Galt\Bundle\TicketBundle\Entity\Ticket $authored
+     */
+    public function removeAuthored(\Galt\Bundle\TicketBundle\Entity\Ticket $authored)
+    {
+        $this->authored->removeElement($authored);
     }
 }
